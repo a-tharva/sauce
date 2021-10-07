f4319083265a7292578d695280db7a4f110e1907
Atharva Bhandvalkar <70326109+a-tharva@users.noreply.github.com>
2021-10-07 14:57:58 +0530

    Delete sauce directory

--- sauce/main.py
+++
@@ -1,7 +0,0 @@
-import sauce
-
-#img address
-url = input("Input the url for img search:")
-#search result will be stored in txt file
-filename = input("Name the file:")
-sauce.search_that(url, filename)
