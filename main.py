c54a90d3a6460440110864c2de9d33ed0f6db366
atharva bhandvalkar <atharv.bhandvalkar@gmail.com>
2021-10-13 13:00:58 +0530

    duck

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     print('\n\n                 welcome to sauce')
     print('\n              What do you want to search')
     
+    
     while True:
         try:
             
@@ -17,30 +18,26 @@ def main():
             choice = input('>').lower()
             
             if choice == 'image':
-                #img address
-                url = input(">>Input the url for img search:")
-
-                #search result will be stored in txt file
-                filename = input(">>Name the file:")
-
-                #function call
+                url = input(">>Input the url for img search:")  #img address
+                filename = input(">>Name the file:")            #search result will be stored in txt file
                 sauce.search_that(url, filename)
                 
+            
             elif choice == 'wiki':
-                
                 wiki = input('>>Input wiki search tearm:')
                 sauce.wiki_that(wiki)
                 
+            
             elif choice == 'company':
-                
                 wiki = input('>>Input wiki company to search:')
                 
                 if 'company' not in wiki:
                     wiki = wiki + ' company'
+                
                 sauce.wiki_that(wiki)
             
+            
             elif choice == 'duck':
-                
                 search_for = input('>>Search for: ')
                 sauce.duckduckgo(search_for)
                 
@@ -49,6 +46,7 @@ def main():
             print('\nExit..')
             break
     
+    
+    
 if __name__ == '__main__':
     main()
-    
