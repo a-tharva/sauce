e0de5c5e2537cb1fc34f01b136e4c95c9a4f86e0
atharva bhandvalkar <atharv.bhandvalkar@gmail.com>
2021-11-08 21:52:27 +0530

    format change

--- main.py
+++ main.py
@@ -2,46 +2,32 @@
 
 menu for sauce 
 """
-
 import sauce
 
-def main():
 
+def main():
     print('\n\n                 welcome to sauce')
     print('\n              What do you want to search')
-    
-    
     while True:
         try:
-            
             print("\n--Image  \n--Wiki \n--Company \n--Duck   Go DuckDuckGo it")
             choice = input('>').lower()
-            
             if choice == 'image':
                 url = input(">>Input the url for img search:")  #img address
                 filename = input(">>Name the file:")            #search result will be stored in txt file
-                sauce.search_that(url, filename)
-                
-            
+                sauce.search_that(url, filename)    
             elif choice == 'wiki':
-                wiki = input('>>Input wiki search tearm:')
-                sauce.wiki_that(wiki)
-                
-            
+                wiki_search = input('>>Input wiki search tearm:')
+                sauce.wiki(wiki_search)
             elif choice == 'company':
-                wiki = input('>>Input wiki company to search:')
-                
-                if 'company' not in wiki:
-                    wiki = wiki + ' company'
-                
-                sauce.wiki_that(wiki)
-            
-            
+                wiki_search = input('>>Input wiki company to search:')
+                if 'company' not in wiki_search:
+                    wiki_search = wiki_search + ' company'
+                sauce.wiki(wiki_search)
             elif choice == 'duck':
                 search_for = input('>>Search for: ')
                 sauce.duckduckgo(search_for)
-                
-            
+        # Keyboard interrupt ctrl+c        
         except KeyboardInterrupt:
             print('\nExit..')
             break
